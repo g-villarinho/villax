59bb1a29d2d39f12be96d65bca0ad3b4a3cbaadc
Gabriel Villarinho <gabrielvillarinho01@gmail.com>
2025-11-20 17:49:37 -0300

    refactor: update Babel configuration to use Villax as the JSX pragma and enhance .gitignore

--- didact-alias.js
+++ didact-alias.js
@@ -1,6 +1,6 @@
 // Alias global para Didact sem modificar index.js
 // Usa getter para garantir acesso às funções já hoistadas em index.js.
-Object.defineProperty(window, 'Didact', {
+Object.defineProperty(window, 'Villax', {
     configurable: true,
     get() {
         return {
